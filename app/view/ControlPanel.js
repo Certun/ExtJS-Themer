fc62a0d8bc50a056fd0ed5cda9d844ec8fe6fee3
vela1606 <vela1606@gmail.com>
2011-10-01 11:24:22 -0400

    Option panel progress

--- app/view/ControlPanel.js
+++ app/view/ControlPanel.js
@@ -6,40 +6,79 @@
  * To change this template use File | Settings | File Templates.
  */
 Ext.define('App.view.ControlPanel', {
-    extend      : 'Ext.panel.Panel',
+    extend      : 'Ext.form.Panel',
     alias       : 'widget.controlpanel',
     width       : 250,
-    
-    defaults    : {
-        collapsible     : true,
-        titleCollapse   : true,
-        collapsed       : true,
-        frame           : true,
-        margin          : '0 0 5 0',
-        width           : '100%',
-        bodyStyle       : { padding:5 }
-    },
-
     items: [{
-        title:'Title',
-        html:'hello world'
+        xtype       : 'fieldset',
+        defaultType : 'button',
+        layout      : 'column',
+        defaults    : { columnWidth: .33, enableToggle: true, toggleGroup:1 },
+        title       : '1. Choose Base Thame',
+        items:[{
+            text:'Blue',
+            margin:'0 5 0 0'
+        },{
+            text:'Gray',
+            margin:'0 5 0 5'
+        },{
+            text:'Access',
+            margin:'0 0 0 5'
+        }]
     },{
-        title:'Title',
-        html:'hello world'
+        xtype       : 'fieldset',
+        collapsible : false,
+        collapsed   : false,
+        defaults    : {
+            collapsible     : true,
+            titleCollapse   : true,
+            collapsed       : true,
+            frame           : true,
+            margin          : '0 0 5 0',
+            width           : '100%',
+            bodyStyle       : { padding:5 }
+        },
+        title   : '2. Choose Theme Options',
+        items   : [{
+            title:'Fonts',
+            html:'hello world'
+        },{
+            title:'Colors',
+            html:'hello world'
+        },{
+            title:'Corner Radius',
+            html:'hello world'
+        },{
+            title:'Headers Styles',
+            html:'hello world'
+        },{
+            title:'Body Styles',
+            html:'hello world'
+        },{
+            title:'Buttons',
+            html:'hello world'
+        },{
+            title:'Drop Shadows',
+            html:'hello world'
+        },{
+            title:'Erors',
+            html:'hello world'
+        },{
+            title:'Other',
+            html:'hello world'
+        }]
     },{
-        title:'Title',
-        html:'hello world'
-    },{
-        title:'Title',
-        html:'hello world'
-    },{
-        title:'Title',
-        html:'hello world'
-    },{
-        title:'Title',
-        html:'hello world'
-    },{
-        title:'Title',
-        html:'hello world'
+        xtype       : 'fieldset',
+        defaultType : 'button',
+        layout      : 'column',
+        defaults    : { columnWidth: .5 },
+        title       : '3. Preview Changes or Download Theme',
+        items:[{
+            text:'Preview',
+            margin:'0 5 0 0'
+        },{
+            text:'Download',
+            margin:'0 0 0 5'
+        }]
     }]
 });
